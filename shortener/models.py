1fd0003661d06c35756fc1980e762686f653ceb5
Aizyy008 <malikaizan198@gmail.com>
2025-04-21 18:11:32 +0500

    Add short code generator method

--- shortener/models.py
+++ shortener/models.py
@@ -10,10 +10,10 @@ class URL(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     access_count = models.IntegerField(default=0)
 
-    # @classmethod
-    # def generate_short_code(cls, length=6):
-    #     chars = string.ascii_letters + string.digits
-    #     while True:
-    #         short_code = ''.join(random.choice(chars) for _ in range(length))
-    #         if not cls.objects.filter(short_code=short_code).exists():
-    #             return short_code
+    @classmethod
+    def generate_short_code(cls, length=6):
+        chars = string.ascii_letters + string.digits
+        while True:
+            short_code = ''.join(random.choice(chars) for _ in range(length))
+            if not cls.objects.filter(short_code=short_code).exists():
+                return short_code
